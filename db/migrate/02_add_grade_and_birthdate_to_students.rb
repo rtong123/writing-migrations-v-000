a884074c2a7b6df0ec2e1ca5ea62e4ec54f1af76
Rebecca Tong <rtong@buffalo.edu>
2018-05-08 18:16:38 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,7 +1,7 @@
 class AddGradeAndBirthday < ActiveRecord::Migration[5.1]
   def change
     add_column :students, :grade, :integer
-                :students, :birthdate, :string
+    add_column  :students, :birthdate, :string
 
   end
 end
